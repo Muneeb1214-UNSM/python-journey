33703424edb5469641e8c5cf9453dbc9277f22e6
Muneeb Khan <muneebmagsiofficial@gmail.com>
2025-07-30 11:33:02 +0500

    Create Day3.py
    Day 3 of python in which we learn about operators.

---
+++ Day3.py
@@ -0,0 +1,24 @@
+#Operators in Python
+#1 arithmetic operators(+,-,*,/,%)
+a = 10
+b = 20
+print("the sum of a and b is :", a+b)
+print("the difference of a and b is :", a-b)
+print("the multiplication of a and b is :", a*b)
+print("the division of a and b is:", a/b)
+print("the modulus of a and b is:", a%b)
+
+#2 Relational operators (<,>,<=,>=)
+x = 5
+y = 2
+print(x<y)
+print(x==y)
+print(x<=y)
+print(x>=y)
+
+#3 Assignment operators (+=,-=,*=,/=)
+j = 10
+k = 5
+print(j+=k)
+print(j-=k)
+print(j*=k)
